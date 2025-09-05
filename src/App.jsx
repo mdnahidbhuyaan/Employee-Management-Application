78a633eb5d9138de0cd0dc83329901c81ec186ad
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-09-05 18:52:42 +0600

    authproviber

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import React, { useEffect } from 'react'
+/* eslint-disable no-unused-vars */
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage } from './utils/localStorage'
+import { useState } from 'react'
+// import { getLocalStorage } from './utils/localStorage'
 
 const App = () => {
 
+const [user,setUser] = useState(null)
+const handleLogin = (email,password)=>{
+ if(email == "admin@me.com" && password == "123"){
+setUser("admin")
+console.log(user)
+ }else if(email == "user@me.com" && password == "123"){
+   setUser("employee")
+   console.log(user)
+ }
+ else{
+  alert("invalid Caredentials")
+ }
+}
 
-  useEffect(() => {
-  // setLocalStorage()
-  getLocalStorage()
+  // useEffect(() => {
+  // // setLocalStorage()
+  // getLocalStorage()
   
 
-  }, [])
+  // }, [])
   
 
 
 
   return (
 <>
-<Login/>
+{!user ? <Login handleLogin={handleLogin}/> : ""}
+{user == "admin" ? <AdminDashboard/> :<EmployeeDashboard/>}
 {/* <EmployeeDashboard/> */}
 {/* <AdminDashboard/> */}
 
--- src/components/other/TaskListNumber.jsx
+++ src/components/other/TaskListNumber.jsx
@@ -4,7 +4,7 @@ const TaskListNumber = () => {
   return (
     <div className="flex mt-10 justify-between gap-5 screen ">
       <div className=" w-[45%] py-6 px-9  bg-red-400 rounded-xl">
-        <h2 className="text-2xl font-semibold text-white">0</h2>
+        <h2 className="text-2xl font-semibold text-white">1</h2>
         <h3 className="text-xl text-medium text-white">New Task</h3>
       </div>
          <div className=" w-[45%] py-6 px-9  bg-blue-400 rounded-xl">
